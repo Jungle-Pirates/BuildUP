ec0a816bab599b17d38155bb71573af91e75ee3f
ChoiHyoHun <hh04460446@gmail.com>
2025-04-09 11:24:16 +0900

    Feat : 빵, 생고기 먹는 코드 추가

--- Assets/Scripts/HyoHun/Food/BreadItem.cs
+++ Assets/Scripts/HyoHun/Food/BreadItem.cs
@@ -12,6 +12,7 @@ public class BreadItem : FoodItem
     public override void Use(PlayerController user)
     {
         Debug.Log($"{itemName}을 먹고 체력을 {healAmount}만큼 회복했다.");
+        base.Use(user);
         //회복 처리 
         //이 아이템 소비 처리
         //PlayerHealth.Instance.Heal(healAmount);
--- Assets/Scripts/HyoHun/Food/MeatItem.cs
+++ Assets/Scripts/HyoHun/Food/MeatItem.cs
@@ -14,6 +14,7 @@ public class MeatItem : FoodItem
     public override void Use(PlayerController user)
     {
         Debug.Log($"{itemName}을 먹고 체력을 {healAmount}만큼 회복했다.");
+        base.Use(user);
         //회복 처리 
         //이 아이템 소비 처리
         //PlayerHealth.Instance.Heal(healAmount);
